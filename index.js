66f7ed031e588dbfe2db51f1eabfd1108fe24618
gabriel M <gabriel_jm_3@hotmail.com>
2025-07-07 17:40:10 -0300

    gabo commit

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ import fs from 'fs';
 
 const app = express();
 app.use(express.static('public'));
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 // Configurar multer para la carga de imágenes
 const storage = multer.memoryStorage();
